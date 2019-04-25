b6edad4f1a21063eeb88d9ec7ff5165c1c05c04d
Luis Chueneuien Liuzhou <luis.chueneuien@metrica.es>
2019-04-25 15:45:44 +0200

    Añadido un writeline

--- Semita/Program.cs
+++ Semita/Program.cs
@@ -7,7 +7,7 @@ namespace Semita
         public int MyProperty { get; set; } = 6;
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Luis");
             Console.WriteLine("Hello World!");
         }
     }
