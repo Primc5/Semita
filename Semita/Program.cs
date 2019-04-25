dd9b8ff2ce9c9996619128f761ac2b60ea5992ea
Pablo Cruz Mendez <pablo.cruz@metrica.es>
2019-04-25 14:35:52 +0200

    init check version of c#, 6 or supirior

---
+++ Semita/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Semita
+{
+    class Program
+    {
+        public int MyProperty { get; set; } = 6;
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
